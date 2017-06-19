dafbbcd7d062a9f4a49b6688f9aa0ee544bd93f9
BenJohnCarson <benjohncarson@gmail.com>
2017-06-19 14:47:38 +0100

    added db settings and task model

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'em-tasks',
     environment: environment,
+    contentSecurityPolicy: {'connect-src': "'self' wss://*.firebaseio.com"},
+    firebase: 'https://emtasks-187ed.firebaseio.com/',
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
